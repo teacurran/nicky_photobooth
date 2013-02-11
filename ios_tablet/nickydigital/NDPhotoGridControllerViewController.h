8ab733b8929bf1caf7f64b60018c03d3f430c5ca
Terrence Curran <tea@approachingpi.com>
2013-02-11 00:06:27 -0500

    gridviewcontroller

---
+++ ios_tablet/nickydigital/NDPhotoGridControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  NDPhotoGridControllerViewController.h
+//  nickydigital
+//
+//  Created by Terrence Curran on 2/10/13.
+//  Copyright (c) 2013 Terrence Curran. All rights reserved.
+//
+
+#import <Pods/Pods.h>
+
+@interface NDPhotoGridControllerViewController : BDDynamicGridViewController
+
+@end
---
+++ ios_tablet/nickydigital/NDPhotoGridControllerViewController.m
@@ -0,0 +1,38 @@
+//
+//  NDPhotoGridControllerViewController.m
+//  nickydigital
+//
+//  Created by Terrence Curran on 2/10/13.
+//  Copyright (c) 2013 Terrence Curran. All rights reserved.
+//
+
+#import "NDPhotoGridControllerViewController.h"
+
+@interface NDPhotoGridControllerViewController ()
+
+@end
+
+@implementation NDPhotoGridControllerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
